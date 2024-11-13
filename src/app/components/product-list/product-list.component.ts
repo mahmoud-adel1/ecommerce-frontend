0e61bf00e906711f7e6c793e308373d7899b8cd9
mahmoud-077 <mahmoudadel537@gmail.com>
2024-11-13 16:14:33 +0200

    Search by category

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
 import { CommonModule, NgFor } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -15,15 +16,27 @@ import { CommonModule, NgFor } from '@angular/common';
 export class ProductListComponent implements OnInit{
   
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) { }
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe( ()=>{
+      this.listProducts();
+    } )
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
+    }
+    
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data=>{
         this.products = data;
       }
