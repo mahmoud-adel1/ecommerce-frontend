a89cdef6432c4cf49756803a820c4589553fdaf4
mahmoud-077 <mahmoudadel537@gmail.com>
2024-12-15 09:11:18 +0200

    access to secured order history url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,18 @@ import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } fr
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { OKTA_CONFIG, OktaAuthModule, OktaAuthStateService, OktaCallbackComponent } from '@okta/okta-angular';
 import OktaAuth from '@okta/okta-auth-js';
 import myAppConfig from './config/my-app-config';
-import OktaSignIn from '@okta/okta-signin-widget';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 const oktaAuth = new OktaAuth(myAppConfig.oidc);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(withInterceptorsFromDi()),
               {provide: OKTA_CONFIG, useValue: {oktaAuth}},OktaAuthStateService,OktaCallbackComponent,
+              {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true},
               importProvidersFrom(OktaAuthModule)
   ]
 };
