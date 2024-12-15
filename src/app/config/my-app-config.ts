8a7cd59a4ad106f89829b8ca3414f34d34166fa7
mahmoud-077 <mahmoudadel537@gmail.com>
2024-12-16 01:41:18 +0200

    convert to https

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oaln732y49LvFoda5d7',
         issuer: 'https://dev-66220591.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile','email']
     }
 }
